4ac0e07fd62e803efafd8e1ab78cded7632d94ff
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-04-13 09:49:15 +0700

    edit static picture

--- src/components/Portoflio.jsx
+++ src/components/Portoflio.jsx
@@ -9,8 +9,7 @@ export default function Portoflio() {
       url: 'http://eralintasdata.co.id/',
       description:
         'Website company profile untuk bisnis yang bergerak di bidang konsultan IT, website yang dibangun dengan menggunakan standar pemrograman web dan dibantu dengan framework bootstrap CSS agar mendapatkan hasil yang sesuai dengan kebutuhan',
-      image:
-        'https://media-exp1.licdn.com/dms/image/C562DAQHbdTZ7VYYq7w/profile-treasury-image-shrink_800_800/0/1632104569484?e=1649494800&v=beta&t=kgiIkx4Jq8Ke89525i486abPmdCQ-x-fjqRXXWkNdUw',
+      image: '../assets/image/porto/eralintas.jfif',
       date: new Date('2020-09-01'),
       tech: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
     },
@@ -19,8 +18,7 @@ export default function Portoflio() {
       url: 'https://cper-fwd-fundamental.netlify.app/',
       description:
         'Situs web yang menyajikan data film menggunakan API TMDB API. Website yang dibangun hanya fokus pada fetch API dan slicing design',
-      image:
-        'https://media-exp1.licdn.com/dms/image/C562DAQHy-dUkCNtuQQ/profile-treasury-image-shrink_800_800/0/1649305808303?e=1649494800&v=beta&t=ESGujo7bm2lDtQIhTaOoWzJuQeLUcfWMXryONXXm5rg',
+      image: '../assets/image/porto/movienesia.png',
       date: new Date('2021-09-01'),
       tech: ['HTML', 'SCSS', 'JavaScript', 'TMDB API'],
     },
@@ -29,8 +27,7 @@ export default function Portoflio() {
       url: 'https://cperdiansyah-fwd-sub3.netlify.app/',
       description:
         'Website yang menyajikan data restoran dan menu-menu di dalamnya. CRUD dengan fetch API, mendukung Progressive Web App (PWA), dan berkinerja baik pada PageSpeed Insights',
-      image:
-        'https://media-exp1.licdn.com/dms/image/C562DAQEZvzJrSw5JFQ/profile-treasury-image-shrink_800_800/0/1649305817750?e=1649494800&v=beta&t=NBWGSGaq-NIQSn_d9PDWpxUvGYeezRlqOxeTsMSPqrQ',
+      image: '../assets/image/porto/makanin.png',
       date: new Date('2021-12-01'),
       tech: [
         'HTML',
@@ -46,15 +43,14 @@ export default function Portoflio() {
       url: 'https://www.sobatgadaiku.com/',
       description:
         'Website penggadaian properti dan aset yang dibangun menggunakan wordpress dengan banyak kustomisasi desain menggunakan tailwind css serta berfokus pada SEO agar muncul pada halaman awal pencarian google',
-      image:
-        'https://media-exp1.licdn.com/dms/image/C562DAQEO31aSh37Oog/profile-treasury-image-shrink_800_800/0/1649305233239?e=1649494800&v=beta&t=7fRa0HnRvBAykPk9-KWtjcn84-srHBUSgRMUCro0nWI',
+      image: '../assets/image/porto/SobatGadaiku.png',
       date: new Date('2021-12-01'),
       tech: ['Wordpress', 'Tailwind CSS', 'JavaScript'],
     },
   ];
 
   const dataPortoflioSorted = dataPortoflio.sort((a, b) => b.date - a.date);
-  
+
   return (
     <section id='portfolio' className='pt-36 pb-16 bg-slate-100'>
       <div className='container'>
